7665a2f5f3c42de1886fcadf5459cdead47ecebe
Gerd Müller <gerdmuller.de@gmail.com>
2021-04-25 21:14:28 +0200

    changed upload script

--- assets/upload_v1.0.3.bat
+++
@@ -1,3 +0,0 @@
-#python -m esptool --before default_reset --after hard_reset --chip esp8266 --baud 460800 write_flash 0x0 cointhing_v1.0.3.bin 0x200000 spiffs_v1.0.3.bin
-
-python -m esptool --before default_reset --after hard_reset --chip esp8266 --baud 921600 write_flash 0x0 cointhing_v1.0.3.bin 0x200000 spiffs_v1.0.3.bin
