beebeadb1f4fd4e17372016f7cb64ab26a8903f0
Gerd Müller <gerdmuller.de@gmail.com>
2021-07-26 18:08:52 +0200

    fits in 2MB SPIFFS again

--- auxiliaries/server/cointhing.js
+++ auxiliaries/server/cointhing.js
@@ -8,10 +8,10 @@ var port = 3000
 
 server.listen(port, function () {
     console.log('CoinThing server port: %d', port)
-    console.log('Serve path: ' + __dirname + '/../../source/data')
+    console.log('Serve path: ' + __dirname + '/../../source')
 })
 
-app.use(express.static(__dirname + '/../../source/data'))
+app.use(express.static(__dirname + '/../../source'))
 app.get('/action/set', function (req, res) {
 
     for (const [key, value] of Object.entries(req.query)) {
